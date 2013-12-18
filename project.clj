5bbceef6245544b8daba1c2e8315f83bc136089f
dmitryame <dmitry@custommode.com>
2013-12-17 23:28:28 -0500

    hiccup removed

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
-                 [hiccup "1.0.4"]
                  [ring-server "0.3.1"]
                  [mysql/mysql-connector-java "5.1.6"]
                  [korma "0.3.0-RC6"]
