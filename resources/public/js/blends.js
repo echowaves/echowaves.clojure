0e83c7a7a222327905de629ef1bfd46cd951b562
dmitryame <dmitry@custommode.com>
2013-12-23 21:07:21 -0500

    WIP autocomplete

--- resources/public/js/blends.js
+++ resources/public/js/blends.js
@@ -23,3 +23,40 @@ $.getJSON( "/unconfirmed-blends.json", function( data ) {
     $("#unconfirmed-blends").append(items);
 });
 
+
+
+$( "#wave_name" ).autocomplete({
+    source: function( request, response ) {
+        $.ajax({
+            url: "/autocomplete-wave-name.json",
+            dataType: "json",
+            data: {
+                featureClass: "P",
+                style: "full",
+                maxRows: 12,
+                name_startsWith: request.term
+            },
+            success: function( data ) {
+                response( $.map( data.geonames, function( item ) {
+                    return {
+                        label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
+                        value: item.name
+                    }
+                }));
+            }
+        });
+    },
+    minLength: 2,
+    select: function( event, ui ) {
+        log( ui.item ?
+             "Selected: " + ui.item.label :
+             "Nothing selected, input was " + this.value);
+    },
+    open: function() {
+        $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+    },
+    close: function() {
+        $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+    }
+});
+
