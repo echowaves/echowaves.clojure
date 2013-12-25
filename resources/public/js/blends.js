76bbae38461a6e750fd70c36bfd08ee567761f2d
dmitryame <dmitry@custommode.com>
2013-12-24 23:27:43 -0500

    refreshing blends properly

--- resources/public/js/blends.js
+++ resources/public/js/blends.js
@@ -1,59 +1,69 @@
-$.getJSON( "/requested-blends.json", function( data ) {
-    var items = [];
-    $.each( data, function( index, item ) {
-        items.push( "<li class='requested-blend' id='" + item.wave_id2 + "'>" + item.name + "</li>" );
+function update_requested_blends() {
+    $("#requested-blends").empty();
+    $.getJSON( "/requested-blends.json", function( data ) {
+        var items = [];
+        $.each( data, function( index, item ) {
+            items.push( "<li class='requested-blend' id='" + item.wave_id2 + "'>" + item.name + "</li>" );
+        });
+        $("#requested-blends").append(items);
     });
-    $("#requested-blends").append(items);
-});
+}
 
-
-$.getJSON( "/blended-with.json", function( data ) {
-    var items = [];
-    $.each( data, function( index, item ) {
-        items.push( "<li id='" + item.id + "'>" + item.name + "</li>" );
+function update_blended_with() {
+    $("#blended-with").empty();
+    $.getJSON( "/blended-with.json", function( data ) {
+        var items = [];
+        $.each( data, function( index, item ) {
+            items.push( "<li id='" + item.id + "'>" + item.name + "</li>" );
+        });
+        $("#blended-with").append(items);
     });
-    $("#blended-with").append(items);
-});
+}
 
-$.getJSON( "/unconfirmed-blends.json", function( data ) {
-    var items = [];
-    $.each( data, function( index, item ) {
-        items.push( "<li class='unconfirmed-blend' id='" + item.wave_id2 + "'>" + item.name + "</li>" );
+function update_unconfirmed_blends() {
+    $("#unconfirmed-blends").empty();
+    $.getJSON( "/unconfirmed-blends.json", function( data ) {
+        var items = [];
+        $.each( data, function( index, item ) {
+            items.push( "<li class='unconfirmed-blend' id='" + item.wave_id2 + "'>" + item.name + "</li>" );
+        });
+        $("#unconfirmed-blends").append(items);
     });
-    $("#unconfirmed-blends").append(items);
-});
+}
 
+function update_all_blends() {
+    update_requested_blends();
+    update_blended_with();
+    update_unconfirmed_blends();
+    $("#wave_name").val("");
+}
 
 
 $(function() {
-    function complete_selection( message ) {
-        $( "<div>" ).text( message ).prependTo( "#requested-blends" );
-    }
     $( "#wave_name" ).autocomplete({
         source: "/autocomplete-wave-name.json",
         minLength: 2,
         select: function( event, ui ) {
-            alert(ui.item.value);
             $.ajax({
                 type: "POST",
                 url: "/request-blending.json",
-                // The key needs to match your method's input parameter (case-sensitive).
                 data: JSON.stringify({wave_name: ui.item.label}),
                 contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                success: function(data){alert(data);},
+                dataType: "json"
+                ,
+                success: function(data){
+                    // alert(data);
+                    update_all_blends();
+                },
                 failure: function(errMsg) {
-                    alert(errMsg);
+                    // alert(errMsg);
+                    update_all_blends();
                 }
             });
-
-            complete_selection( ui.item ?
-                 "Selected: " + ui.item.label :
-                 "Nothing selected, input was " + this.value );
 
 
         }
     });
 });
 
-
+update_all_blends();
