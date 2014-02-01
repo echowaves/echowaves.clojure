01d402a51c36b924deb7c05514f0e9af91da2d95
dmitryame <dmitry@custommode.com>
2014-02-01 11:19:51 -0500

    added generate-token func

--- src/echowaves/util.clj
+++ src/echowaves/util.clj
@@ -11,3 +11,6 @@
 (defn random-string [length]
   (let [ascii-codes (concat (range 48 58) (range 66 91) (range 97 123))]
     (apply str (repeatedly length #(char (rand-nth ascii-codes))))))
+
+(defn generate-token []
+  (random-string 255))
