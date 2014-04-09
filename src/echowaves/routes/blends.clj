68fad768646f3f532757ae31c282aa3a06efafee
dmitryame <dmitry@custommode.com>
2014-04-08 22:54:38 -0400

    blending for multiwaves works

--- src/echowaves/routes/blends.clj
+++ src/echowaves/routes/blends.clj
@@ -18,48 +18,61 @@
       (noir.response/json (db/blended-with (:id wave)))
       (noir.response/status 401 (noir.response/json {:status "unathorized"})))))
 
-(defn handle-requiested-blends []
-  (let [wave (db/get-wave (session/get :wave))]
-    (noir.response/json (db/requested-blends (:id wave)))))
-(defn handle-unconfirmed-blends []
-  (let [wave (db/get-wave (session/get :wave))]
-    (noir.response/json (db/unconfirmed-blends (:id wave)))))
+(defn handle-requested-blends [wave_name]
+  (let [wave (db/get-wave wave_name)]
+    (if (u/check-child-wave wave_name)
+      (noir.response/json (db/requested-blends (:id wave)))
+      (noir.response/status 401 (noir.response/json {:status "unathorized"})))
+    ))
+
+(defn handle-unconfirmed-blends [wave_name]
+  (let [wave (db/get-wave wave_name)]
+    (if (u/check-child-wave wave_name)
+      (noir.response/json (db/unconfirmed-blends (:id wave)))
+      (noir.response/status 401 (noir.response/json {:status "unathorized"})))))
+
 (defn handle-autocomplete-wave-name [term]
   (debug "autocompleting: " term)
   (noir.response/json (db/autocomplete-wave-name term)))
 
-(defn handle-request-blending [wave_name]
-  (debug "requesting blending for: " wave_name)
-  (let [wave1 (db/get-wave (session/get :wave))
+(defn handle-request-blending [wave_name from_wave]
+  (let [wave1 (db/get-wave from_wave)
         wave2 (db/get-wave wave_name)]
-    (u/send-push-notification
-     (str (:name wave1) " wants to blend with " (:name wave2))
-     (str 1) 
-     (db/get-tokens-for-wave (:name wave2)))
-    (noir.response/json {:status (db/request-blending (:id wave1) (:id wave2))})))
-(defn handle-confirm-blending [wave_name]
-  (debug "confirming blending " wave_name)
-  (let [wave1 (db/get-wave (session/get :wave))
+    (if (u/check-child-wave from_wave)
+      (do
+        (u/send-push-notification
+         (str (:name wave1) " wants to blend with " (:name wave2))
+         (str 1) 
+         (db/get-tokens-for-wave (:name wave2)))
+        (noir.response/json {:status (db/request-blending (:id wave1) (:id wave2))}))
+      (noir.response/status 401 (noir.response/json {:status "unathorized"})))))
+
+(defn handle-confirm-blending [wave_name from_wave]
+  (let [wave1 (db/get-wave from_wave)
         wave2 (db/get-wave wave_name)]
-    (noir.response/json {:status (db/confirm-blending (:id wave1) (:id wave2))})))
-(defn handle-unblend [wave_name]
-  (debug "unblending: " wave_name)
-  (let [wave1 (db/get-wave (session/get :wave))
+    (if (u/check-child-wave from_wave)
+      (noir.response/json {:status (db/confirm-blending (:id wave1) (:id wave2))})
+      (noir.response/status 401 (noir.response/json {:status "unathorized"})))))
+
+(defn handle-unblend [wave_name from_wave]
+  (let [wave1 (db/get-wave from_wave)
         wave2 (db/get-wave wave_name)]
-    (noir.response/json {:status (db/unblend (:id wave1) (:id wave2))})))
+    (if (u/check-child-wave from_wave)
+      (noir.response/json {:status (db/unblend (:id wave1) (:id wave2))})
+      (noir.response/status 401 (noir.response/json {:status "unathorized"})))))
 
 (defroutes blends-routes
   (GET "/blended-with.json" [wave_name]
        (restricted (handle-blended-with wave_name)))
-  (GET "/requested-blends.json" []
-       (restricted (handle-requiested-blends)))
-  (GET "/unconfirmed-blends.json" []
-       (restricted (handle-unconfirmed-blends)))
+  (GET "/requested-blends.json" [wave_name]
+       (restricted (handle-requested-blends wave_name)))
+  (GET "/unconfirmed-blends.json" [wave_name]
+       (restricted (handle-unconfirmed-blends wave_name)))
   (GET "/autocomplete-wave-name.json" [term]
        (restricted (handle-autocomplete-wave-name term)))
-  (POST "/request-blending.json" [wave_name]
-        (restricted (handle-request-blending wave_name)))
-  (POST "/confirm-blending.json" [wave_name]
-        (restricted (handle-confirm-blending wave_name)))
-  (POST "/unblend.json" [wave_name]
-       (restricted (handle-unblend wave_name))))
+  (POST "/request-blending.json" [wave_name from_wave]
+        (restricted (handle-request-blending wave_name from_wave)))
+  (POST "/confirm-blending.json" [wave_name from_wave]
+        (restricted (handle-confirm-blending wave_name from_wave)))
+  (POST "/unblend.json" [wave_name from_wave]
+       (restricted (handle-unblend wave_name from_wave))))
