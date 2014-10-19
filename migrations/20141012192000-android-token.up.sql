67f9ea5956aae172df69f3cc293f11e81ceed7d2
dmitryame <dmitry@custommode.com>
2014-10-18 22:12:31 -0400

    modified migration

--- migrations/20141012192000-android-token.up.sql
+++ migrations/20141012192000-android-token.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE android_tokens (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     waves_id INT NOT NULL,
-    token LONGBLOB NOT NULL,
+    token VARCHAR(255) NOT NULL,
     created_on TIMESTAMP DEFAULT NOW()
-) ENGINE=MyISAM;
+);
 
 CREATE INDEX android_tokens_index_created_on ON android_tokens (created_on);
 CREATE INDEX android_tokens_index_wave_id ON android_tokens (waves_id);
-CREATE INDEX android_tokens_index_token ON android_tokens (token(1000));
+CREATE INDEX android_tokens_index_token ON android_tokens (token);
